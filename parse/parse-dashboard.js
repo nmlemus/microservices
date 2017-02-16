94436b78c3d486b7bcbb5c48a6b73a5b1f276ec5
nmlemus <nmlemus@gmail.com>
2017-02-16 18:26:09 -0200

    Changes in the configuration of the parse-dashboard.

--- parse/parse-dashboard.js
+++ parse/parse-dashboard.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var ParseDashboard = require('parse-dashboard');
 
-var dashboard = new ParseDashboard({
+var config = {
+  "allowInsecureHTTP": true,
   "apps": [
     {
-      "serverURL": "http://localhost:1337/parse",
+      "serverURL": "http://69.89.12.148:1337/parse",
       "appId": "Goblob",
       "masterKey": "cassandra",
       "appName": "Goblob"
     }
+  ],"users": [
+    {
+      "user":"goblob",
+      "pass":"goblob"
+    }
   ]
-});
+};
+var dashboard = new ParseDashboard(config, config.allowInsecureHTTP);
 
 var app = express();
 
